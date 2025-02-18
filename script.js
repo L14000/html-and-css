66442c5dffdff36da4b90b2b3b673b1d37d8ba58
L14000 <khawaraltaf@ymail.com>
2025-02-18 11:03:04 +0000

    debugging for error messages

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
   
   
   
-  // New Year Banner
+  // New announcment Banner
 { const banner = document.createElement('div');
-    banner.classList.add('new-year-banner');
+    banner.classList.add('new-announcment-banner');
     banner.innerHTML = "<p>Site is still under construction!</p>";
     document.body.appendChild(banner);
 }
 
-
+//adding image in the background
 
 document.body.style.backgroundImage = "url('tor7.jpg')"; // Add a background image to the body
 document.body.style.backgroundSize = "cover"; // Set the background image to cover the body
